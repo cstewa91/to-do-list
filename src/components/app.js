4abf13194172cddf217fe26e5d5c3195d3c9996c
Collin Stewart <cstewa91@gmail.com>
2018-11-08 14:58:43 -0800

    added axios

--- src/components/app.js
+++ src/components/app.js
@@ -2,39 +2,44 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize';
 import '../assets/css/app.css';
 import React, { Component } from 'react';
+import axios from 'axios';
 import List from './list';
 import AddItem from './add_item';
-import listData from '../dummy_data/list';
 import { randomString } from '../helpers';
 
+const BASE_URL = 'http://api.reactprototypes.com/todos';
+const API_KEY = '?key=c19_cstewa91';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: []
+            list: [],
+            error: ''
         }
     }
-    addItem = (item) => {
-        item._id = randomString(8);
-        this.setState({
-            list: [item, ...this.state.list]
-        });
+    addItem = async (item) => {
+        const resp = await axios.post(BASE_URL + API_KEY, item)
+        this.getListData()
     }
-    deleteItem = (index) => {
-        const listCopy = this.state.list.slice()
-        listCopy.splice(index, 1);
-        this.setState({
-            list: listCopy
-        });
+    deleteItem = async (id) => {
+        const resp = await axios.delete(`${BASE_URL}/${id + API_KEY}`)
+        this.getListData()
     }
     componentDidMount() {
         this.getListData();
     }
-    getListData() {
-        this.setState({
-            list: listData
-        });
+    async getListData() {
+        try {
+            const resp = await axios.get(BASE_URL + API_KEY);
+            this.setState({
+                list: resp.data.todos
+            })
+        } catch (err) {
+            this.setState({
+                error: 'Error getting todos'
+            })
+        }
     }
     render() {
         return (
